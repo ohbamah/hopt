fe868015693b9861776ca6e82ed30096eed8326b
OhBama (Yann Manchon) <60485785+ohbamah@users.noreply.github.com>
2025-04-06 11:58:10 +0200

    Update 4-LLVMPATH.bat

--- WindowsHOPT/4-LLVMPATH.bat
+++ WindowsHOPT/4-LLVMPATH.bat
@@ -20,7 +20,7 @@ IF /i "%_in%"=="y" (
 
 	echo Copy of ..\includes\hopt.h to %_llvm_path%\include\
 	copy ..\includes\hopt.h "%_llvm_path%\include\"
-	echo Copy of .\libhopt.dll to %_llvm_path%\lib\
+	echo Copy of .\libhopt.lib to %_llvm_path%\lib\
 	copy .\libhopt.lib "%_llvm_path%\lib\"
 )
 
